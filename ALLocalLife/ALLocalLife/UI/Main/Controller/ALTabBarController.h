6346a2f8dfd91b0917cd845670cfe299a120579b
alan <1174172121@qq.com>
2015-11-21 19:30:03 +0800

    Init project

---
+++ ALLocalLife/ALLocalLife/UI/Main/Controller/ALTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  ALTabBarController.h
+//  ALLocalLife
+//
+//  Created by 王可成 on 15/11/21.
+//  Copyright © 2015年 AL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALTabBarController : UITabBarController
+
+@end
